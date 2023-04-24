974962378fa5bc205648d01d422c2d93c993a71f
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-04-24 23:21:50 +0400

    create Student.cs file

---
+++ ClassInherutance-Task/Models/Student.cs
@@ -0,0 +1,44 @@
+﻿namespace ClassInherutance_Task.Models;
+
+internal class Student
+{
+    public string Name;
+    public string Surname;
+    public string Group;
+    public int Point;
+    public bool isGraduated;
+
+    public Student(string name, string surname, string group, int point, bool IsGraduated)
+    {
+        Name = name;
+        Surname = surname;
+        Group = group;
+        Point = point;
+        isGraduated = IsGraduated;
+    }
+
+    public string FullName()
+    {
+        return $"{Name} {Surname}";
+    }
+    public string Info()
+    {
+        if (isGraduated == true)
+        {
+
+            return $"{FullName()} {Group} {Point} {isGraduated} Mezun olub";
+        }
+        else
+        {
+            return $"{FullName()} {Group} {Point} {isGraduated} Mezun olmayib";
+        }
+    }
+    public string Exam()
+    {
+        if (Point > 80)
+        {
+            return $"Baliniz {Point} dir, ikinci imtahana gire bilersiniz";
+        }
+        return $"Baliniz {Point} dir, ikinci imtahana gire bilmezsiniz";
+    }
+}
---
+++ ClassInherutance-Task/Program.cs
@@ -0,0 +1,6 @@
+﻿using ClassInherutance_Task.Models;
+
+Student stu1 = new Student("Nicat", "Muxtarov", "P137", 27, false);
+Console.WriteLine(stu1.FullName());
+Console.WriteLine(stu1.Info());
+Console.WriteLine(stu1.Exam());
